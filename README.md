62423cd9dddf5e99545e786e0a60c3ce7797e53b
Guy Duchatelet <guy.duchatelet@spyderbat.com>
2023-08-04 14:55:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# Spyderbat kubernetes deployment tools
+
+This repository containers tooling to support the deployment and validation process of the spyderbat monitoring solution.
+
+## cluster_collect.py - Spyderbat pre-deployment collection script.
+
+In order to optimally configure and size the Spyderbat collection agent and backend to
+support your kubernetes cluster, we have created a script that will collect some useful data and metrics
+that Spyderbat can review to optimize the Helm installation or our agents, and size our backend appropriately.
+
+
+### What does the script collect:
+----------------------------
+1. summary metrics about the nr of nodes, pods, deployments, replicasets, daemonsets, services and namespaces
+
+    This helps us assess the size and load on your cluster
+
+2. information about the nodes of the cluster, including their provisioned capacity and any taints applied to the nodes
+
+    This helps us understand the headroom available in your cluster to add our agents, and helps us pro-actively recommend
+    configuring tolerations on our agents to ensure visibility on all nodes
+
+3. Cumulative metrics about what resource requests currently running pods are requesting (cpu, memory)
+
+    This helps us understand the headroom available in your cluster to add our agents.
+
+4. The name and namespaces of the deployments, daemonsets and services running on your cluster
+
+    This helps us assess if any other daemonsets or deployments could interfere with our agents and helps us discover
+    if your cluster has node-auto-scaling configured
+
+5. PriorityClasses currently present for the cluster
+
+    This helps us assess whether our agent will have sufficient priority to get scheduled on any new nodes being added
+    to the cluster
+
+
+### What this script does NOT collect:
+
+We do not collect implementation and status details in the 'spec' and 'status' sections of the pods, deployments or daemonsets.
+No sensitive data that might be present in these sections of the k8s resources (environment variables, configs) is collected.
+
+
+### Requirements
+
+This script should be run from a machine you currently use to manage your cluster from.
+
+The following are required
+
+1. python3.7 or higher
+    https://www.python.org/downloads/
+
+2. kubectl and a valid kube config file
+    https://kubernetes.io/docs/tasks/tools/
+
+    The script will call on the kubectl command to collect cluster information. The cluster(s) to install Spyderbat on
+    should be one of the contexts configured in the kube config file.
+
+
+### Usage
+After installing the script run it as
+./cluster_collect.py -h
+    OR
+python3 cluster_collect.py -h
+
+for usage info
+usage: cluster_collect.py [-h] [-k KUBECONFIG] [-c CONTEXT] [-o OUTPUT]
+
+options:
+  -h, --help            show this help message and exit
+  -k KUBECONFIG, --kubeconfig KUBECONFIG
+                        path to kubeconfig file (if omitted, default is ~/.kube/config)
+  -c CONTEXT, --context CONTEXT
+                        kubectl context to pull from (if none provided, all contexts in the kubectl config will be analyzed)
+  -o OUTPUT, --output OUTPUT
+                        output file (default is spyderbat-clusterinfo.json.gz)
+
+By default, the script will collect information for all clusters configured in your kubeconfig file.
+If you want to collect only for a single one, use the -c CONTEXT flag, with the name of the context
+(as available in kubectl config get-contexts) to collect for.
+
+For example:
+./cluster_collect.py -c qacluster1
+
+by default the output will go into a file called spyderbat-clusterinfo.json.gz
+You can use the -o flag to use another filename.
+
+
+### After collection
+If the script ran successfully, please send the output file back to Spyderbat.
+We will review the findings with you to discuss the next steps for your deployment.
+
---
+++ cluster_collect.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+import argparse
+import gzip
+import json
+import subprocess
+import sys
+from collections import defaultdict
+
+resource_types = ["namespaces", "nodes", "pods", "deployments", "replicasets", "daemonsets", "services", "priorityclasses"]
+
+"""
+A utility to collect non-sensitive sizing information about one or more
+kubernetes clusters to optimize the Spyderbat deployment process of agents
+on the cluster, and appropriately size the agents and Spyderbat backend.
+"""
+
+def summarize_cluster(index, index_schema):
+    cluster_summary: dict[str, int] = {}
+    cluster_summary['nr_nodes'] = len(index_schema['Node'])
+    cluster_summary['nr_pods'] = len(index_schema['Pod'])
+    cluster_summary['nr_deployments'] = len(index_schema['Deployment'])
+    cluster_summary['nr_replicasets'] = len(index_schema['ReplicaSet'])
+    cluster_summary['nr_daemonsets'] = len(index_schema['DaemonSet'])
+    cluster_summary['nr_services'] = len(index_schema['Service'])
+    cluster_summary['nr_namespaces'] = len(index_schema['Namespace'])
+    return cluster_summary
+
+
+def get_node_usage(index, index_schema):
+    node_usage = defaultdict(dict)
+    nodes = index_schema['Node'].values()
+    pods = index_schema['Pod'].values()
+    for node in nodes:
+        provider = 'unknown'
+        for label in node['metadata']['labels']:
+            if "aws" in label or "amazon" in label:
+                provider = "AWS EKS"
+                break
+            if "gke" in label or "google" in label:
+                provider = "GCP GKE"
+                break
+            if "azure" in label:
+                provider = "Azure AKS"
+                break
+
+        node_usage[node['metadata']['name']] = {
+            "arch": node['status']['nodeInfo']['architecture'],
+            "osImage": node['status']['nodeInfo']['osImage'],
+            "containerRuntime": node['status']['nodeInfo']['containerRuntimeVersion'],
+            "instance_type": node['metadata']['labels'].get('node.kubernetes.io/instance-type', "unknown"),
+            "cores": int(node['status']['capacity']['cpu']),
+            "capacity_pods": int(node['status']['capacity']['pods']),
+            "usage_pods": 0,
+            "headroom_pod": 0,
+            "capacity_memory": convert_unit(node['status']['capacity']['memory']),
+            "usage_memory": 0,
+            "headroom_memory": 0,
+            "capacity_cpu": convert_unit(node['status']['capacity']['cpu']),
+            "usage_cpu": 0,
+            "headroom_cpu": 0,
+            "taints": node['spec'].get('taints', []),
+            "control_plane": node['metadata']['labels'].get('node-role.kubernetes.io/controlplane', False),
+            "provider": provider
+        }
+
+    for pod in pods:
+        node_name = pod['spec'].get('nodeName')
+        if node_name:
+            node_usage[node_name]['usage_pods'] += 1
+            for container in pod['spec']['containers']:
+                if 'resources' not in container:
+                    continue
+                if 'requests' not in container['resources']:
+                    continue
+                node_usage[node_name]['usage_memory'] += convert_unit(container['resources']['requests'].get('memory',0))
+                node_usage[node_name]['usage_cpu'] += convert_unit(container['resources']['requests'].get('cpu',0))
+    return node_usage
+
+
+def convert_unit(amount):
+    if type(amount) == int:
+        return amount
+    stripped = amount.strip()
+    if stripped.endswith('Ki'):
+        return int(stripped[:-2])*1024
+    if stripped.endswith('Mi'):
+        return int(stripped[:-2])*1024*1024
+    if stripped.endswith('Gi'):
+        return int(stripped[:-2])*1024*1024*1024
+    if stripped.endswith('Ti'):
+        return int(stripped[:-2])*1024*1024*1024*1024
+    if stripped.endswith('Pi'):
+        return int(stripped[:-2])*1024*1024*1024*1024*1024
+    if stripped.endswith('Ei'):
+        return int(stripped[:-2])*1024*1024*1024*1024*1024*1024
+    if stripped.endswith('K'):
+        return int(stripped[:-1])*1000
+    if stripped.endswith('M'):
+        return int(stripped[:-1])*1000*1000
+    if stripped.endswith('G'):
+        return int(stripped[:-1])*1000*1000*1000
+    if stripped.endswith('T'):
+        return int(stripped[:-1])*1000*1000*1000*1000
+    if stripped.endswith('P'):
+        return int(stripped[:-1])*1000*1000*1000*1000*1000
+    if stripped.endswith('E'):
+        return int(stripped[:-1])*1000*1000*1000*1000*1000*1000
+    if stripped.endswith('m'):
+        return int(stripped[:-1])/1000
+    return int(stripped)
+
+
+def condense_resources(cluster_data, kind):
+    resources = cluster_data['index_schema'].get(kind, {})
+    return [{"name": r['metadata']['name'],
+             "namespace": r['metadata']['namespace'],
+             "uid": r['metadata']['uid']
+            } for r in resources.values()]
+
+
+def condense(cluster_index):
+    rv = {}
+    for cluster_name, cluster_data  in cluster_index.items():
+        cluster_summary = summarize_cluster(cluster_data['index'], cluster_data['index_schema'])
+        node_usage = get_node_usage(cluster_data['index'], cluster_data['index_schema'])
+        deployments = condense_resources(cluster_data, 'Deployment')
+        daemonsets = condense_resources(cluster_data, 'DaemonSet')
+        services = condense_resources(cluster_data, 'Service')
+        pods = condense_resources(cluster_data, 'Pod')
+
+        # deps = cluster_data['index_schema'].get('Deployment', {})
+        # deployments = [{"name": dep['metadata']['name'], "namespace": dep['metadata']['namespace'], "replicas": dep['spec']['replicas']} for dep in deps.values()]
+        # daems = cluster_data['index_schema'].get('DaemonSet', {})
+        # daemonsets = [{"name": d['metadata']['name'], "namespace": d['metadata']['namespace'], } for d in daems.values()]
+        # svcs = cluster_data['index_schema'].get('Service', {})
+        # services = [{"name": d['metadata']['name'], "namespace": d['metadata']['namespace'] } for d in svcs.values()]
+        # pods = cluster_data['index_schema'].get('Pod', {})
+        # services = [{"name": r['metadata']['name'], "namespace": r['metadata']['namespace'], "uid": r['metadata']['uid'] } for r in pods.values()]
+
+        rv[cluster_name] = {
+            "cluster_summary": cluster_summary,
+            "node_usage": node_usage,
+            "deployments": deployments,
+            "daemonsets": daemonsets,
+            "services": services,
+            "pods": pods,
+            "nodes": cluster_data['index_schema'].get('Node', {}),
+            "priority_classes": cluster_data['index_schema'].get('PriorityClass', {})
+        }
+    return rv
+
+
+def load(args):
+    kubeconfig = f"--kubeconfig {args.kubeconfig}" if args.kubeconfig else ""
+
+    if args.context is None:
+        cmd = f"kubectl {kubeconfig} config get-contexts -o name"
+        contexts = subprocess.check_output(cmd, shell=True).decode('utf-8').split('\n')[:-1]
+    else:
+        contexts = [args.context]
+
+    cluster_index = defaultdict(dict)
+
+    for context in contexts:
+        print(f'collecting data for {context}...', file=sys.stderr)
+        index_schema = defaultdict(dict)
+        index = {}
+
+        for resource_type in resource_types:
+            cmd = f"kubectl {kubeconfig} --context {context} get {resource_type} -A -o json"
+            try:
+                result = subprocess.check_output(cmd, shell=True)
+                resources = json.loads(result)
+                for res in resources['items']:
+                    uid = res['metadata']['uid']
+                    index_schema[res['kind']][uid] = res
+                    index[uid] = res
+            except subprocess.CalledProcessError:
+                print(f'failed to get {resource_type} from {context}, skipping')
+                pass
+        if len(index) != 0:
+            cluster_index[context]['index'] = index
+            cluster_index[context]['index_schema'] = index_schema
+    return cluster_index
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--kubeconfig",
+                        help="path to kubeconfig file (if omitted, default is ~/.kube/config)")
+    parser.add_argument("-c", "--context",
+                        help="kubectl context to pull from (if none provided, all contexts in the kubectl config will be analyzed)",
+                        required=False)
+    parser.add_argument("-o", "--output",
+                        help="output file (default is spyderbat-clusterinfo.json.gz)",
+                        default = "spyderbat-clusterinfo.json.gz")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    cluster_index = load(args)
+    cluster_condensed = condense(cluster_index)
+    with gzip.open(args.output, 'wt') as f:
+        json.dump(cluster_condensed, f)
+        print('Done. Output written to', args.output, file=sys.stderr)
