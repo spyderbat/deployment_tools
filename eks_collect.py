b3e56c9cf549c74d51a0bb83e0b64407db124f71
Guy Duchatelet <guy.duchatelet@spyderbat.com>
2023-08-23 15:24:16 -0400

    add cluster parameter

--- eks_collect.py
+++ eks_collect.py
@@ -3,15 +3,18 @@ import sys
 import boto3
 import json
 
-def get_nodegroup_info(region):
+def get_nodegroup_info(region, cluster_name = 'all'):
     eks = boto3.client('eks', region_name = region)
     info=[]
-    cluster_response = eks.list_clusters()
-    status_code = cluster_response['ResponseMetadata']['HTTPStatusCode']
-    if status_code != 200:
-        print(f'Got response code {status_code} calling list_clusters api')
-        return [{"error": cluster_response}]
-    clusters = cluster_response.get("clusters", [])
+    if cluster_name != 'all':
+        clusters = [cluster_name]
+    else:
+        cluster_response = eks.list_clusters()
+        status_code = cluster_response['ResponseMetadata']['HTTPStatusCode']
+        if status_code != 200:
+            print(f'Got response code {status_code} calling list_clusters api')
+            return [{"error": cluster_response}]
+        clusters = cluster_response.get("clusters", [])
     for cluster in clusters:
         nodegroups_response = eks.list_nodegroups(clusterName = cluster)
         status_code = nodegroups_response['ResponseMetadata']['HTTPStatusCode']
@@ -33,8 +36,9 @@ def get_nodegroup_info(region):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Get EKS nodegroup info')
     parser.add_argument('--region', default='us-east-1', help='AWS region')
+    parser.add_argument('-c', '--eks_cluster_name', default = 'all', help='EKS cluster name (default is all clusters)')
     parser.add_argument('-o', '--output', default='stdout', help='Output file')
     args = parser.parse_args()
-    info = get_nodegroup_info(args.region), open(args.output, 'w')
+    info = get_nodegroup_info(args.region, args.eks_cluster_name)
     output = sys.stdout if args.output == 'stdout' else open(args.output, 'w')
-    json.dump(get_nodegroup_info(args.region), output, default=str)
+    json.dump(info, output, default=str)
